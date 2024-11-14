942bb1d2068895e59e71dd997f5da3de32158deb
Daniela Dinis <2221953@my.ipleiria.pt>
2024-11-14 11:54:57 +0000

    Criação do projeto frontend

---
+++ pmei-client/app.vue
@@ -0,0 +1,8 @@
+<template>
+    <h2>Students</h2>
+    <div>Hello World</div>
+    <button>Refresh Data</button>
+</template>
+<script setup>
+//const { data: students, error, refresh } = await useFetch('http://localhost:8080/academics/api/student')
+</script>
