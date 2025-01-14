4dfd17f1833d48d677a97f0c8c30ca4cb31e3322
André Guerra <2221460@my.ipleiria.pt>
2025-01-14 20:42:05 +0000

    Update [id].client.vue

---
+++ pmei/src/main/java/pt/ipleiria/estg/dei/ei/dae/pmei/dtos/VolumeEstadoDTO.java
@@ -0,0 +1,29 @@
+package pt.ipleiria.estg.dei.ei.dae.pmei.dtos;
+
+import pt.ipleiria.estg.dei.ei.dae.pmei.entities.Produto;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class VolumeEstadoDTO implements Serializable {
+
+    private String estado;
+
+    public VolumeEstadoDTO() {
+    }
+
+    public VolumeEstadoDTO(long idVolume, String estado) {
+
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
