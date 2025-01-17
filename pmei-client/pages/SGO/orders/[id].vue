1631f82e7c333b169a78d952491bff6a4406b2f8
Daniela Dinis <2221953@my.ipleiria.pt>
2025-01-17 20:30:53 +0000

    Verificação sensores

--- pmei-client/pages/SGO/orders/[id].vue
+++ pmei-client/pages/SGO/orders/[id].vue
@@ -8,11 +8,14 @@ const error = ref('')
 const {token, user} = storeToRefs(authStore)
 const order = ref(null)
 
+const config = useRuntimeConfig()
+const api = config.public.API_URL
+
 async function fetch() {
 
   try {
     // fetch the order
-    order.value = await $fetch(`http://localhost:8080/PMEI/monitorizacao/api/encomendas/${route.params.id}`, {
+    order.value = await $fetch(`${api}/encomendas/${route.params.id}`, {
       headers: {
         Authorization: `Bearer ${token.value}`
       }
@@ -41,10 +44,10 @@ const getSensorStatus = (status) => {
 }
 
 const styleStatusBadge = (status) => {
-  if (status === true) {
+  if (status === true || status === 'Entregue') {
     return ['px-2 py-1 text-xs rounded-full bg-green-100 text-green-800'];
   }
-  else if (status === false) {
+  else if (status === false || status === 'Pendente') {
     return ['px-2 py-1 text-xs rounded-full bg-yellow-100 text-yellow-800'];
   }
   else {
@@ -86,6 +89,26 @@ onBeforeMount(() => {
 
       <h1 v-if="order" class="text-2xl md:text-3xl font-bold mb-6 text-gray-800">Encomenda #{{ order.encomendaId }}</h1>
 
+      <div v-if="order" class="bg-white rounded-lg shadow-md p-6 mb-5">
+          <h1 class="text-2xl font-bold text-gray-800 pb-4">Detalhes da Encomenda</h1>
+          <div class="grid grid-cols-3 gap-4">
+            <div>
+              <p class="text-lg font-semibold text-gray-800">ID Cliente</p>
+              <p class="text-sm text-gray-600">{{ order.customerId }}</p>
+            </div>
+            <div>
+              <p class="text-lg font-semibold text-gray-800">Estado</p>
+              <p class="text-sm text-gray-600">
+                <span :class="styleStatusBadge(order.estado)">{{ order.estado }}</span>
+              </p>
+            </div>
+            <div>
+              <p class="text-lg font-semibold text-gray-800">Entregue</p>
+              <p class="text-sm text-gray-600">{{ isDelivered(order.entregue) }}</p>
+            </div>
+          </div>
+      </div>
+
       <!-- Sensores -->
       <div v-if="order" class="bg-white rounded-lg shadow-md p-6">
         <div class="border-b pb-4 mb-4">
@@ -98,7 +121,7 @@ onBeforeMount(() => {
                   <p class="text-sm text-gray-600 ml-2 font-medium">Tipo da Embalagem: {{ volume.tipoEmbalagem }}</p>
                   <p class="text-sm text-gray-600 ml-2 font-medium">Entregue: {{ isDelivered(volume) }}</p>
                 </div>
-                <table class="min-w-full">
+                <table v-if="volume.sensores.length > 0" class="min-w-full">
                   <thead>
                   <tr class="bg-gray-100">
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID Sensor</th>
@@ -122,6 +145,11 @@ onBeforeMount(() => {
                   </tr>
                   </tbody>
                 </table>
+                <!-- No sensors yellow warning-->
+                <div v-else class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded mt-4">
+                  Não foram encontrados sensores para este volume.
+                </div>
+
               </div>
 
           </div>
